adf589ec9557b650d250592de49274e69ac429c8
Collinda Fahey <collindafahey@gmail.com>
2020-12-30 18:53:27 -0400

    separated out image card into its own component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,35 +1,18 @@
 import styles from "../../styles/Board.module.css";
+import Card from "./Card.js";
 
 export default function Board(props) {
   return (
     <div className={styles.grid}>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
-      <div className={styles.card}>
-        <img src={props.data[Math.floor(Math.random() * 10)].urls.small} />
-      </div>
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
+      <Card imgSrc={props.data[Math.floor(Math.random() * 10)].urls.small} />
     </div>
   );
 }
