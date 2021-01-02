e4c47ce055983bb31997bd09744fda170c9f3238
Collinda Fahey <collindafahey@gmail.com>
2021-01-02 14:51:37 -0400

    set array to determine images for board on initial load of app

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,26 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import sampleSize from "lodash.samplesize";
 import shuffle from "lodash.shuffle";
 import styles from "../../styles/Board.module.css";
 import Card from "./Card.js";
 
-const getArrayOfIndexes = () => {
+const getArrayOfIndexes = (indexArray) => {
   // return an array of 6 random number pairs between 0 & 11
-  const origIndexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  const tempIndexArray = sampleSize(origIndexArray, 6);
+  const tempIndexArray = sampleSize(indexArray, 6);
   const indexPairsArray = tempIndexArray.concat(tempIndexArray);
 
   return indexPairsArray;
 };
 
 export default function Board(props) {
-  const [indexArray, setIndexArray] = useState();
-  const newArray = getArrayOfIndexes();
-  const shuffledArray = shuffle(newArray);
+  const [indexArray, setIndexArray] = useState([
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+  ]);
+
+  useEffect(() => {
+    // Set array that determines images for board only on initial load of app
+    const newArray = getArrayOfIndexes(indexArray);
+    const shuffledArray = shuffle(newArray);
+    setIndexArray(shuffledArray);
+  }, []);
 
   return (
     <div className={styles.grid}>
-      {shuffledArray.map((item, index) => (
+      {indexArray.map((item, index) => (
         <Card key={index} imgSrc={props.data[item].urls.small} />
       ))}
     </div>
