17b46e46686619e20df0e24e14ebaf9ed39debfa
Collinda Fahey <collindafahey@gmail.com>
2021-01-02 18:02:52 -0400

    optimized logic for assigning indexes to boards

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import useSWR from "swr";
-import Board from "../src/components/Board.js";
+import Board from "../src/components/Board";
+import { useCallback, useState } from "react";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Home() {
   // get photos from proxy
   const { data, error } = useSWR("/api/image", fetcher);
+
+  const [count, setCount] = useState(0);
+  const [attempts, setAttempts] = useState(0);
+
+  // increase attempt by 1
+  const increaseAttempts = useCallback(() => {
+    setAttempts((prevAttempts) => prevAttempts + 1);
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -25,7 +34,7 @@ export default function Home() {
           <Board data={data.images.results} />
         )}
         <div className={styles.aside}>
-          <p>Attempts: 0</p>
+          <p>Attempts: {attempts}</p>
         </div>
       </main>
       <footer className={styles.footer}>
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,38 +2,20 @@ import { useEffect, useState } from "react";
 import sampleSize from "lodash.samplesize";
 import shuffle from "lodash.shuffle";
 import styles from "../../styles/Board.module.css";
-import Card from "./Card.js";
+import Card from "./Card";
 
-const getArrayOfIndexes = (indexArray) => {
+const getArrayOfIndexes = () => {
   // return an array of 6 random number pairs between 0 & 11
-  const tempIndexArray = sampleSize(indexArray, 6);
+  const originalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const tempIndexArray = sampleSize(originalArray, 6);
   const indexPairsArray = tempIndexArray.concat(tempIndexArray);
+  const shuffledArray = shuffle(indexPairsArray);
 
-  return indexPairsArray;
+  return shuffledArray;
 };
 
 export default function Board(props) {
-  const [indexArray, setIndexArray] = useState([
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-  ]);
-
-  useEffect(() => {
-    // Set array that determines images for board only on initial load of app
-    const newArray = getArrayOfIndexes(indexArray);
-    const shuffledArray = shuffle(newArray);
-    setIndexArray(shuffledArray);
-  }, []);
+  const [indexArray] = useState(() => getArrayOfIndexes());
 
   return (
     <div className={styles.grid}>
