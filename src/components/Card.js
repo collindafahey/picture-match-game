58ff6f0ca9d423b1917b8efdae2335c060f45ce7
Collinda Fahey <collindafahey@gmail.com>
2021-01-02 22:05:49 -0400

    added flipping functionality on click

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
+import { useState, useCallback } from "react";
 import styles from "../../styles/Card.module.css";
 
 export default function Card(props) {
-  const isFlipped = true;
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const onClick = useCallback(() => {
+    props.increaseCount();
+    setIsFlipped((flipped) => !flipped);
+  }, []);
+
   return (
-    <div className={styles.card} onClick={props.increaseCount}>
+    <div className={styles.card} onClick={onClick}>
       {isFlipped && <img className={styles.img} src={props.imgSrc} />}
     </div>
   );
